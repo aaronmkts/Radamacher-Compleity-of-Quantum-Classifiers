7160ea29803f8a3b64099cef13343ea0bad159c0
Aaron Mark Thomas <aaronmkts@gmail.com>
2024-10-10 13:52:10 +0100

    update

--- model/__init__.py
+++ model/__init__.py
@@ -1,1 +1,16 @@
-from .reuploading import QML_classifier
+from .reuploading import Reuploading_classifier
+from .angle import AngleEmbeddingClassifier
+from .amplitude import AmplitudeEmbeddingClassifier
+
+
+
+def select_classifier(name: str):
+    match name:
+        case "reuploading":
+            return Reuploading_classifier()
+        case "angle":
+            return AngleEmbeddingClassifier()
+        case "amplitude":
+            return AmplitudeEmbeddingClassifier()
+        case _:
+            raise ValueError(f"Unknown classifier: {name}")
