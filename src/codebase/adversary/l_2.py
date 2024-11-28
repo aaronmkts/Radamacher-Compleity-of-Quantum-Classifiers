6f1320b8e0dfb9e4ab1f839452efdbdd6800138d
Aaron Mark Thomas <aaronmkts@gmail.com>
2024-11-28 13:44:09 +0000

    adv train

--- src/codebase/adversary/l_2.py
+++ src/codebase/adversary/l_2.py
@@ -11,16 +11,20 @@ def L2(model, inputs, labels, loss_func, epsilon):
     # Forward pass
     outputs = model(inputs_adv)
     outputs = outputs.view(-1)
-
+    
     # Compute loss
     loss = loss_func(outputs, labels_adv)
+  
 
     # Backward pass
     loss.backward()
 
     # Update adversarial inputs
+
     with torch.no_grad():
-        # Apply gradient ascent to maximize the loss
-        inputs_adv += epsilon * inputs_adv.grad()/torch.linalg.norm(inputs_adv.grad())
+        grad = inputs_adv.grad  
+        grad_norm = torch.linalg.norm(grad, dim=1, keepdim=True) + 1e-8  
+        perturbation = epsilon * grad / grad_norm  
+        inputs_adv = inputs_adv + perturbation  # Apply perturbation
 
     return inputs_adv.detach()
